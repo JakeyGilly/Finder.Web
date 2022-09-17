ba71d6654a34f08535a05e3517a878913b3aa99b
Jake Gillman <57807696+JakeyGilly@users.noreply.github.com>
2022-09-17 17:40:28 +0100

    finsished docker support for now

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,11 @@
-﻿FROM mcr.microsoft.com/dotnet/aspnet:6.0 AS base
+﻿FROM mcr.microsoft.com/dotnet/sdk:6.0
+COPY . /app
 WORKDIR /app
-EXPOSE 80
-EXPOSE 443
-
-FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
-WORKDIR /src
-COPY *.csproj ./
-RUN dotnet restore
-COPY . .
-WORKDIR "/src/"
-RUN dotnet build -c Release -o /app/build
-
-FROM build AS publish
-RUN dotnet publish -c Release -o /app/publish
-
-FROM base AS final
-WORKDIR /app
-COPY --from=publish /app/publish .
-ENTRYPOINT ["dotnet", "Finder.Web.dll"]
+ENV ASPNETCORE_URLS=http://+:80
+RUN ["dotnet", "restore"]
+RUN ["dotnet", "build"]
+EXPOSE 80/tcp
+RUN ["dotnet", "tool", "install", "--global", "dotnet-ef"]
+ENV PATH="${PATH}:~/.dotnet/tools/"
+RUN chmod +x ./entrypoint.sh
+CMD /bin/bash ./entrypoint.sh
--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 using Finder.Web.Database;
-using Finder.Web.Models;
 using Finder.Web.Repositories.Bot;
 using Finder.Web.Repositories.Web;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
 
@@ -29,40 +31,34 @@ public static class Program {
             return;
         }
         var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddDbContext<ApplicationContext>(options =>
-            options.UseNpgsql(Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")!),
-            ServiceLifetime.Transient
-        );
+        builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")!), ServiceLifetime.Transient);
         builder.Services.AddScoped<UserSettingsRepository>();
         builder.Services.AddScoped<AddonsRepository>();
-        builder.Services.AddAuthentication(options => {
-            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        }).AddCookie(options => {
-            options.LoginPath = "/signin";
-            options.ExpireTimeSpan = TimeSpan.FromDays(7);
-        }).AddDiscord(options => {
-            options.Scope.Add("identify");
-            options.Scope.Add("guilds");
-            options.Prompt = "none";
-            options.ClientId = Environment.GetEnvironmentVariable("DISCORD_CLIENT_ID")!;
-            options.ClientSecret = Environment.GetEnvironmentVariable("DISCORD_CLIENT_SECRET")!;
-            options.SaveTokens = true;
-            options.Events = new OAuthEvents {
-                OnCreatingTicket = context => {
-                    List<string> ownerIds = Environment.GetEnvironmentVariable("BOT_OWNER_IDS")?.Split(',').ToList() ?? new();
-                    if (!ulong.TryParse(context.Principal.FindFirstValue(ClaimTypes.NameIdentifier), out var userId)) return Task.CompletedTask;
-                    if (ownerIds.IsNullOrEmpty() || !ownerIds.Contains(userId.ToString())) {
-                        context.Identity.AddClaim(new Claim("IsBotOwner", "false"));
-                    } else {
-                        context.Identity.AddClaim(new Claim("IsBotOwner", "true"));
+        builder.Services.AddAuthentication(options => options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options => {
+                options.LoginPath = "/signin";
+                options.ExpireTimeSpan = TimeSpan.FromDays(7);
+            }).AddDiscord(options => {
+                options.Scope.Add("identify");
+                options.Scope.Add("guilds");
+                options.Prompt = "none";
+                options.ClientId = Environment.GetEnvironmentVariable("DISCORD_CLIENT_ID")!;
+                options.ClientSecret = Environment.GetEnvironmentVariable("DISCORD_CLIENT_SECRET")!;
+                options.SaveTokens = true;
+                options.Events = new OAuthEvents {
+                    OnCreatingTicket = context => {
+                        List<string> ownerIds = Environment.GetEnvironmentVariable("BOT_OWNER_IDS")?.Split(',').ToList() ?? new();
+                        if (!ulong.TryParse(context.Principal.FindFirstValue(ClaimTypes.NameIdentifier), out var userId)) return Task.CompletedTask;
+                        if (ownerIds.IsNullOrEmpty() || !ownerIds.Contains(userId.ToString())) {
+                            context.Identity.AddClaim(new Claim("IsBotOwner", "false"));
+                        } else {
+                            context.Identity.AddClaim(new Claim("IsBotOwner", "true"));
+                        }
+                        return Task.CompletedTask;
                     }
-                    return Task.CompletedTask;
-                }
-            };
-        });
-        builder.Services.AddAuthorization(options => {
-            options.AddPolicy("IsBotOwner", policy => policy.RequireClaim("IsBotOwner", "true", "false"));
-        });
+                };
+            });
+        builder.Services.AddAuthorization(options => options.AddPolicy("IsBotOwner", policy => policy.RequireClaim("IsBotOwner", "true", "false")));
         builder.Services.AddHttpClient();
         builder.Services.AddControllersWithViews();
         var app = builder.Build();
@@ -75,3 +71,4 @@ public static class Program {
         app.Run();
     }
 }
+
